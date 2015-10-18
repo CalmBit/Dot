1a72226d63cc9a479472b4c65329860580771ce2
Ethan Brooks <EB5473@gmail.com>
2015-10-17 22:36:17 -0400

    Production

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'forgery'
 
-gem 'dogstatsd-ruby'
+gem 'newrelic_rpm'
 
 gem 'dogapi'
 
--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-rails s --binding=104.236.224.216 -p 80
+#!/bin/bash
+
+if [ "$1" == "--production" ]; then
+    echo "Starting Dot Server (PRODUCTION MODE)"
+    rails s --binding=104.236.224.216 -p 80 -e production
+else
+    echo "Starting Dot Server (DEVELOPMENT MODE)"
+    rails s --binding=104.236.224.216 -p 80
+fi
